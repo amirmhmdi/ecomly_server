3c01bd25d02a4b039480092831b8290b9928ac9e
Amir Mohammadi <amirmhmdi95@gmail.com>
2025-12-08 13:18:49 +0000

    Reservation Cron Job

--- helper/cron_job.js
+++ helper/cron_job.js
@@ -19,3 +19,46 @@ cron.schedule("0 0 0 * * *", async () => {
     }
 
 });
+
+cron.schedule("*/30 * * * *", async () => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        console.log("Reservation Cron Job Start at", new Date());
+
+        const expiredReservations = await CartProduct.find({
+            reserved: true,
+            reservedUntil: { $lte: new Date() }
+        }).session(session);
+
+        for (const cartProduct of expiredReservations) {
+            const product = await Product.findById(cartProduct.product).session(session);
+            if (product) {
+                const updatedProduct = await Product.findByIdAndUpdate(
+                    { _id: product._id },
+                    { $inc: { countInStock: cartProduct.quantity } },
+                    { new: true, runValidators: true, session }
+                );
+                if (!updatedProduct) {
+                    console.log("Error eccured while updating product. potential concurrency issue.");
+                    await session.abortTransaction();
+                    return;
+                }
+            }
+
+            await cartProduct.findByIdAndUpdate(
+                cartProduct._id,
+                { reserved: false },
+                { session }
+            );
+        }
+
+        await session.commitTransaction();
+        console.log("Reservation Cron Job Completed at", new Date());
+    } catch (error) {
+        await session.abortTransaction();
+        console.error("Cron job error :", error);
+    } finally {
+        session.endSession();
+    }
+})
